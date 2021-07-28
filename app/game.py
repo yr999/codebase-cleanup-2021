77332f891d8075bd607f8df695557da0f9561eab
yr999 <62799330+yr999@users.noreply.github.com>
2021-07-27 21:08:58 -0400

    Updated automated testing

--- app/game.py
+++ app/game.py
@@ -1,44 +1,69 @@
 
 from random import choice
+def determine_winner(user_choice, computer_choice):
+    #return "paper"
+    winners = {
+        "rock": {
+            "rock": None,
+            "paper": "paper",
+            "scissors": "rock",
+        },
+        "paper": {
+            "rock": "paper",
+            "paper": None,
+            "scissors": "scissors",
+        },
+        "scissors": {
+            "rock": "rock",
+            "paper": "scissors",
+            "scissors": None,
+        }
+    }
+    winning_choice = winners[user_choice][computer_choice]
+    return winning_choice
+if __name__ == "__main__":
+    # STILL ALLOW US TO RUN FROM COMMAND LINE
+    # AND DO THE STUFF BELOW
+    # BUT NOT DO THE STUFF BELOW WHEN TRYING TO IMPORT
+    # IN ORDER FOR US TO BE ABLE TO IMPORT ANY FUNCTION
+    # ... FROM ANY FILE (LIKE THIS ONE)
+    # ... THIS FILE NEEDS A CLEAN GLOBAL SCOPE
+    # ... (ONLY FUNCTIONS AND A MAIN CONDITIONAL)
+    #
+    # USER SELECTION
+    #
+    u = input("Please choose one of 'Rock', 'Paper', or 'Scissors': ").lower()
+    print("USER CHOICE:", u)
+    if u not in ["rock", "paper", "scissors"]:
+        print("OOPS, TRY AGAIN")
+        exit()
+    #
+    # COMPUTER SELECTION
+    #
+    c = choice(["rock", "paper", "scissors"])
+    print("COMPUTER CHOICE:", c)
+    # DETERMINE WINNER
+    winner = determine_winner(u, c)
+    if winner == u:
+        print("YOU WON!")
+    elif winner == c:
+        print("YOU LOST")
+    else:
+        print("TIE")
 
-#
-# USER SELECTION
-#
 
-u = input("Please choose one of 'Rock', 'Paper', or 'Scissors': ").lower()
-print("USER CHOICE:", u)
-if u not in ["rock", "paper", "scissors"]:
-    print("OOPS, TRY AGAIN")
-    exit()
 
-#
-# COMPUTER SELECTION
-#
 
-c = choice(["rock", "paper", "scissors"])
-print("COMPUTER CHOICE:", c)
 
-#
-# DETERMINATION OF WINNER
-#
 
-if u == "rock" and c == "rock":
-    print("It's a tie!")
-elif u == "rock" and c == "paper":
-    print("The computer wins")
-elif u == "rock" and c == "scissors":
-    print("The user wins")
 
-elif u == "paper" and c == "rock":
-    print("The computer wins")
-elif u == "paper" and c == "paper":
-    print("It's a tie!")
-elif u == "paper" and c == "scissors":
-    print("The user wins")
 
-elif u == "scissors" and c == "rock":
-    print("The computer wins")
-elif u == "scissors" and c == "paper":
-    print("The user wins")
-elif u == "scissors" and c == "scissors":
-    print("It's a tie!")
+
+
+
+
+
+
+
+
+
--- test/game_test.py
+++ test/game_test.py
@@ -1,4 +1,16 @@
 
-# TODO: import some code
-
+from app.game import determine_winner
 # TODO: test the code
+#u = "rock"
+#c = "scissors"
+#winner = "USER" # / rock
+def test_enlarge():
+    assert determine_winner(user_choice="rock", computer_choice="paper") == "paper"
+    assert determine_winner(user_choice="rock", computer_choice="rock") == None
+    assert determine_winner(user_choice="rock", computer_choice="scissors") == "rock"
+    assert determine_winner(user_choice="paper", computer_choice="paper") == None
+    assert determine_winner(user_choice="paper", computer_choice="rock") == "paper"
+    assert determine_winner(user_choice="paper", computer_choice="scissors") == "scissors"
+    assert determine_winner(user_choice="scissors", computer_choice="paper") == "scissors"
+    assert determine_winner(user_choice="scissors", computer_choice="rock") == "rock"
+    assert determine_winner(user_choice="scissors", computer_choice="scissors") == None
